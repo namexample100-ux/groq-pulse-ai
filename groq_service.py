205a4119aa4a743dc9e6cefa6b593fea45edb760
Alexey Ivanov <namexample100@gmail.com>
2026-02-24 01:06:20 +0500

    feat: upgraded vision to next-gen Llama 4 Scout model

--- groq_service.py
+++ groq_service.py
@@ -183,7 +183,7 @@ class GroqService:
         try:
             response = await self.client.chat.completions.create(
                 messages=temp_history,
-                model="llama-3.2-90b-vision-preview",
+                model="meta-llama/llama-4-scout-17b-16e-instruct",
             )
             ai_response = response.choices[0].message.content
             
